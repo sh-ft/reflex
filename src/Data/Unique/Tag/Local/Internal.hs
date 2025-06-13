517a48d67834ae47c4521a94e7cff2d73bb06bb9
sh-ft <voidproxy@gmail.com>
2025-06-13 21:56:34 +0200

    Remove dlist dependency

--- src/Data/Unique/Tag/Local/Internal.hs
+++ src/Data/Unique/Tag/Local/Internal.hs
@@ -14,7 +14,7 @@ import Data.Primitive.MutVar
 import Data.GADT.Compare
 import Data.Some
 import GHC.Exts (Int (..), Int#, MutVar#, unsafeCoerce#)
-import Type.Reflection
+import Data.Typeable ((:~:)(Refl))
 
 import Unsafe.Coerce
 
